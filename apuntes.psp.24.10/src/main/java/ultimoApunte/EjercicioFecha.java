aa17d90eabe294bc81181bc1cfb009a2196d05ac
Daniel1998-cenec <danielcesar1998@hotmail.com>
2023-11-02 22:07:54 +0100

    actividad fecha realizada :dog:

--- apuntes.psp.24.10/src/main/java/ultimoApunte/EjercicioFecha.java
+++ apuntes.psp.24.10/src/main/java/ultimoApunte/EjercicioFecha.java
@@ -1,57 +1,66 @@
 package ultimoApunte;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
-import java.text.SimpleDateFormat;
-import java.text.ParseException;
 
 public class EjercicioFecha {
 
     public static void main(String[] args) throws IOException {
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingresa la nueva fecha (en formato MM-dd-yyyy):");
-        String nuevaFechaStr = scanner.nextLine();
+        String fecha = scanner.nextLine();
 
-        if (validarFecha(nuevaFechaStr)) {
-            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "date " + nuevaFechaStr);
-
+        ProcessBuilder pb = new ProcessBuilder("cmd", "/C", "date");
+        try {
             Process p = pb.start();
-            InputStream is = p.getInputStream();
-            OutputStream os = p.getOutputStream();
-
-            int c;
-            while ((c = is.read()) != -1) {
-                System.out.print((char) c);
-            }
-
-            System.out.println("Presiona Enter para finalizar:");
-            scanner.nextLine();
-
-            is.close();
-
-            // Espera a que el proceso termine
-            try {
-                int exitCode = p.waitFor();
-                System.out.println("Proceso terminado con código de salida: " + exitCode);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("Fecha no válida. Debe estar en el formato MM-dd-yyyy.");
+            cambiarFecha(p, fecha);
+            p.waitFor();
+            mostrarResultadosDir(p);
+            mostrarErroresDir(p);
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Error al lanzar el comando cmd: " + e.getMessage());
+            System.out.println("El cliente no dispone de los privilegios requeridos.");
         }
     }
 
-    public static boolean validarFecha(String fecha) {
-        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
-        sdf.setLenient(false);
-        try {
-            sdf.parse(fecha);
-            return true;
-        } catch (ParseException e) {
-            return false;
+    private static void cambiarFecha(Process p, String fecha) {
+        try (OutputStream os = p.getOutputStream();
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os))) {
+            bw.write(fecha);
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el proceso: " + e.getMessage());
+        }
+    }
+
+    private static void mostrarErroresDir(Process p) {
+        try (InputStream is = p.getErrorStream();
+             InputStreamReader ir = new InputStreamReader(is, "UTF-8");
+             BufferedReader br = new BufferedReader(ir)) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al mostrar errores del proceso hijo: " + e.getMessage());
+        }
+    }
+
+    private static void mostrarResultadosDir(Process p) {
+        try (InputStream is = p.getInputStream();
+             InputStreamReader ir = new InputStreamReader(is, "UTF-8");
+             BufferedReader br = new BufferedReader(ir)) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al mostrar resultados del proceso hijo: " + e.getMessage());
         }
     }
 }
